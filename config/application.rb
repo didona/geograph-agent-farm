adf584fdde05a199ede720470b663c42eea46bce
ziparo <ziparo@algorithmica.it>
2012-04-18 13:27:21 +0200

    fixed autoload paths

--- config/application.rb
+++ config/application.rb
@@ -45,7 +45,8 @@ module GeographGenerator
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-	 config.autoload_paths += %W(#{config.root}/lib)
+    #config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib", "#{config.root}/lib/**/"]
     # config.autoload_paths += %W(#{config.root}/extras)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
