903e0e6724febe17128a783e689fa56a6d2edc96
root <root@localhost.localdomain>
2013-05-03 09:02:02 -0400

    working on

--- lib/behaviors/random_tester.rb
+++ lib/behaviors/random_tester.rb
@@ -34,18 +34,39 @@ module Behaviors
     def initialize
       super
       @behavior_step = 0
+      @was_running = false
     end
 
     def next_action
-      if @behavior_step % 2 == 0
-        next_pos = get_next_pos
-        Madmass.logger.warn "==[#{self}] #{@behavior_step}: GETTING POSTS NEAR TO #{next_pos[:latitude]}, #{next_pos[:longitude]}"
-        next_action = get_nearby_posts_action next_pos
-      else
-        Madmass.logger.warn "==[#{self}] #{@behavior_step}: GOT PERCEPTION #{Madmass.current_perception.inspect}"
-        next_action = nil
+      next_action = nil
+      if @agent.status == 'running'
+        if not @was_running
+          @behavior_step = 0
+        end
+        @was_running = true
+        if @behavior_step % 2 == 0
+          next_pos = get_next_pos
+          Madmass.logger.warn "==[#{self}] GETTING POSTS NEAR TO #{next_pos[:latitude]}, #{next_pos[:longitude]}"
+          next_action = get_nearby_posts_action next_pos
+        else
+          nearby_posts = Madmass.current_perception[0].data[:nearby_posts]
+          if nearby_posts.empty?
+            Madmass.logger.warn "==[#{self}] no post found nearby: nothing to do"
+          else
+            Madmass.logger.warn "==[#{self}] found #{nearby_posts.size} posts nearby"
+            post = nearby_posts[rand(0..(nearby_posts.size-1))]
+            Madmass.logger.warn "==[#{self}] randomly chosen this: #{post.inspect}"
+            next_action = comment_post_action({ 
+                :latitude => @current_pos[:latitude], 
+                :longitude => @current_pos[:longitude],
+                :post_id => post[:id]
+            })
+          end
+        end
+        @behavior_step += 1
+      elsif @agent.status == 'stopped'
+        @was_running = false
       end
-      @behavior_step += 1
       next_action
     end
 
@@ -59,16 +80,18 @@ module Behaviors
       next_pos = @current_route[@position_in_route]
       @position_in_route += 1
       @current_route = nil if @current_route[@position_in_route].nil?
+      @current_pos = next_pos
       next_pos
     end
 
     def get_nearby_posts_action opts
       lat = opts[:latitude].to_s.to_f + (0.5 - rand) * 0.1 
       lon = opts[:longitude].to_s.to_f + (0.5 - rand) * 0.1 
-      return {
+      result = {
         :cmd => "madmass::action::remote",
         :data => {
           :cmd => 'actions::get_nearby_posts',
+          :sync => true,
           :latitude => lat,
           :longitude => lon,
           :max_count => 10,
@@ -77,19 +100,21 @@ module Behaviors
         }
       }
 
-      Madmass.logger.warn "==[#{self}] created comment post action: \n #{result.to_yaml}\n"
+      Madmass.logger.warn "==[#{self}] created get nearby posts action: \n #{result.to_yaml}\n"
       return result
     end
 
     def comment_post_action opts
-      lat = opts[:latitude].to_s.to_f + (0.5 - rand) * 0.1 
-      lon = opts[:longitude].to_s.to_f + (0.5 - rand) * 0.1 
-      return {
+      lat = opts[:latitude].to_s.to_f
+      lon = opts[:longitude].to_s.to_f
+      post_id = opts[:post_id]
+      result = {
         :cmd => "madmass::action::remote",
         :data => {
           :cmd => 'actions::comment_post',
           :latitude => lat,
           :longitude => lon,
+          :post_id => post_id,
 #          :data => {
 #            :type => "Post",
 #            :body => "Post from Blogger\n, Coordinates <#{lon},#{lat}>"
@@ -103,14 +128,16 @@ module Behaviors
     end
 
     def like_post_action opts
-      lat = opts[:latitude].to_s.to_f + (0.5 - rand) * 0.1 
-      lon = opts[:longitude].to_s.to_f + (0.5 - rand) * 0.1 
-      return {
+      lat = opts[:latitude].to_s.to_f
+      lon = opts[:longitude].to_s.to_f
+      post_id = opts[:post_id]
+      result = {
         :cmd => "madmass::action::remote",
         :data => {
           :cmd => 'actions::like_post',
           :latitude => lat,
           :longitude => lon,
+          :post_id => post_id,
           :user => { :id => @agent.id }
         }
       }
